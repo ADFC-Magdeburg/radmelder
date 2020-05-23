4daf26cec099eda5c2cb49772c76af73b454e010
ABMDcyc <b.arno82@gmx.de>
2020-05-24 00:48:52 +0200

    #1 modified vote saving: ip-address is saved md5 encoded (GDPR), so that multiple votes for same problem of same address can be identified. also change query for vote counter.

--- php/vote.php
+++ php/vote.php
@@ -1,14 +1,32 @@
 <?php
 include("config.php");
 
+/* IP ermitteln*/
+function getUserIpAddr(){
+    if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+        //ip from share internet
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+        //ip pass from proxy
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }else{
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
+
+$ipadresse=md5(getUserIpAddr());
+
+
 $mysql = new mysqli($host, $user, $password, $database);
 $mysql->set_charset("utf8");
 
 $data = json_decode(file_get_contents('php://input'), true);
 $id = $data['id'];
 
-$mysql->query("INSERT INTO votes (fk_stelle) VALUES ($id)");
-$query_text = "SELECT COUNT(*) AS supported FROM votes WHERE fk_stelle=$id GROUP BY fk_stelle;";
+$mysql->query("INSERT INTO votes (fk_stelle,ip_hash) VALUES ($id,'".$ipadresse."')");
+//$query_text = "SELECT COUNT(*) AS supported FROM votes WHERE fk_stelle=$id GROUP BY fk_stelle;";
+$query_text = "SELECT COUNT(*) AS supported FROM (select fk_stelle from votes where fk_stelle=$id group by ip_hash) as a WHERE fk_stelle=$id GROUP BY fk_stelle;";
 //echo $query_text;
 $query = $mysql->query($query_text);
 $result=array();
