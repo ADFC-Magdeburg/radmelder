147fba093fecffa085e1e5af8130fe3f0d3a826a
ABMDcyc <b.arno82@gmx.de>
2020-05-24 00:19:44 +0200

    #1 changed HTTP to HTTPS

--- js/angular-touch.js
+++ js/angular-touch.js
@@ -14,7 +14,7 @@
  *
  * The `ngTouch` module provides touch events and other helpers for touch-enabled devices.
  * The implementation is based on jQuery Mobile touch event handling
- * ([jquerymobile.com](http://jquerymobile.com/)).
+ * ([jquerymobile.com](https://jquerymobile.com/)).
  *
  *
  * See {@link ngTouch.$swipe `$swipe`} for usage.
