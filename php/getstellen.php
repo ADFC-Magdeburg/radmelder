022c2cf26e07d0890d4c45db0b48ab5b21e0527a
ABMDcyc <b.arno82@gmx.de>
2020-05-24 17:46:54 +0200

    #1 Added timestamp to query, so that ordering can be done by date and not number of votes. Also modified query in respect to IP hash to count correct number of votes.

--- php/getstellen.php
+++ php/getstellen.php
@@ -6,12 +6,16 @@ $mysql->set_charset("utf8");
 $result=array();
 $markers=array();
 
-$query= $mysql->query("SELECT stellen.*, b.supported FROM stellen LEFT JOIN (SELECT fk_stelle AS id, COUNT(*) AS supported FROM votes GROUP BY fk_stelle ORDER BY supported DESC) AS b ON b.id=stellen.id WHERE stellen.published=1 ORDER BY b.supported DESC;");
+$query= $mysql->query("SELECT stellen.id, stellen.lat, stellen.lng, stellen.position_text, stellen.Titel, stellen.Problem, stellen.Loesung,stellen.Bild, stellen.Status, stellen.published, stellen.declined, DATE_FORMAT(stellen.timestamp, '%d.%m.%Y') AS timestamp, b.supported FROM stellen LEFT JOIN (SELECT fk_stelle AS id, COUNT(*) AS supported FROM (select fk_stelle,ip_hash from votes group by ip_hash,fk_stelle)as reduziert GROUP BY fk_stelle ORDER BY supported DESC) AS b ON b.id=stellen.id WHERE stellen.published=1 and stellen.declined=0");
+//Order hat hier keine direkte Wirkung. Dies kommt daher, da irgendwo im JavaScript die ID als String interpretiert wird und dann z.B. 8, 80, 81 sortiert wird
+
+
 $i=0;
 while($t=$query->fetch_assoc()) {
   $markers[$i]=$t;
   ++$i;
 }
 $result['markers']=$markers;
+
 echo json_encode($result);
 ?>
