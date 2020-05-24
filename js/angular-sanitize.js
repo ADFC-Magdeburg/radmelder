551de8f2123e05c65698bbefa3ebe32a9805e1ff
ABMDcyc <b.arno82@gmx.de>
2020-05-24 18:25:11 +0200

    #1 replaced http by https

--- js/angular-sanitize.js
+++ js/angular-sanitize.js
@@ -555,11 +555,11 @@ angular.module('ngSanitize', []).provider('$sanitize', $SanitizeProvider);
            .controller('ExampleController', ['$scope', function($scope) {
              $scope.snippet =
                'Pretty text with some links:\n'+
-               'http://angularjs.org/,\n'+
+               'https://angularjs.org/,\n'+
                'mailto:us@somewhere.org,\n'+
                'another@somewhere.org,\n'+
                'and one more: ftp://127.0.0.1/.';
-             $scope.snippetWithTarget = 'http://angularjs.org/';
+             $scope.snippetWithTarget = 'https://angularjs.org/';
            }]);
        </script>
        <div ng-controller="ExampleController">
@@ -598,32 +598,32 @@ angular.module('ngSanitize', []).provider('$sanitize', $SanitizeProvider);
      <file name="protractor.js" type="protractor">
        it('should linkify the snippet with urls', function() {
          expect(element(by.id('linky-filter')).element(by.binding('snippet | linky')).getText()).
-             toBe('Pretty text with some links: http://angularjs.org/, us@somewhere.org, ' +
+             toBe('Pretty text with some links: https://angularjs.org/, us@somewhere.org, ' +
                   'another@somewhere.org, and one more: ftp://127.0.0.1/.');
          expect(element.all(by.css('#linky-filter a')).count()).toEqual(4);
        });
 
        it('should not linkify snippet without the linky filter', function() {
          expect(element(by.id('escaped-html')).element(by.binding('snippet')).getText()).
-             toBe('Pretty text with some links: http://angularjs.org/, mailto:us@somewhere.org, ' +
+             toBe('Pretty text with some links: https://angularjs.org/, mailto:us@somewhere.org, ' +
                   'another@somewhere.org, and one more: ftp://127.0.0.1/.');
          expect(element.all(by.css('#escaped-html a')).count()).toEqual(0);
        });
 
        it('should update', function() {
          element(by.model('snippet')).clear();
-         element(by.model('snippet')).sendKeys('new http://link.');
+         element(by.model('snippet')).sendKeys('new https://link.');
          expect(element(by.id('linky-filter')).element(by.binding('snippet | linky')).getText()).
-             toBe('new http://link.');
+             toBe('new https://link.');
          expect(element.all(by.css('#linky-filter a')).count()).toEqual(1);
          expect(element(by.id('escaped-html')).element(by.binding('snippet')).getText())
-             .toBe('new http://link.');
+             .toBe('new https://link.');
        });
 
        it('should work with the target property', function() {
         expect(element(by.id('linky-target')).
             element(by.binding("snippetWithTarget | linky:'_blank'")).getText()).
-            toBe('http://angularjs.org/');
+            toBe('https://angularjs.org/');
         expect(element(by.css('#linky-target a')).getAttribute('target')).toEqual('_blank');
        });
      </file>
@@ -646,7 +646,7 @@ angular.module('ngSanitize').filter('linky', ['$sanitize', function($sanitize) {
       url = match[0];
       // if we did not match ftp/http/www/mailto then assume mailto
       if (!match[2] && !match[4]) {
-        url = (match[3] ? 'http://' : 'mailto:') + url;
+        url = (match[3] ? 'https://' : 'mailto:') + url;
       }
       i = match.index;
       addText(raw.substr(0, i));
