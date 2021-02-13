4045ff221d425000ddb4a5c3b194667a86f8ded3
ABMDcyc <b.arno82@gmx.de>
2021-02-13 18:07:42 +0100

    #4 Important update in config for dynamic modifications made in earlier release!!!

--- php/config_example.php
+++ php/config_example.php
@@ -4,4 +4,27 @@ $host="localhost";
 $user ="username";
 $password="password";
 $database="database";
+
+
+$uploadpath="radwegmelder/subfolder/upload";//where the images go
+$fulluploadpath='https://domain/radwegmelder/subfolder/upload/';//where the images go, full path with domain etc.
+$verwaltungpath='https://domain/radwegmelder/subfolder/verwaltung/';//where the backend is located. Full path with domain
+$sitepath='https://domain/radwegmelder/subfolder/';//path, where the radmelder is located
+$impressumlink='https://domain/subfolder/file';//link to imprint (can be another site or just html-file)
+$datenschutzlink='https://domain/subfolder/file';//link to gdpr text (can be another site or just html-file)
+$domain='domain';
+
+$sitetitle='ADFC-Radwegmelder';
+
+$cityname='cityname';
+
+$mailsmtp = 'smtp';                   		// SMTP-Server
+$mailuser = 'user@domain';       // SMTP username und gleichzeitig Absendeadresse
+$mailsendername='sendername';
+$mailpasswort = 'password';                         // SMTP Passwort
+$mailport = port;   
+
+
+
+
 ?>
